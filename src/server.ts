9e8c37613b3f41e4a2023741bf37d8dd828e9bb3
Dokhos <mendozaluca5@outlook.com>
2020-09-19 22:39:54 -0300

    Configuración TSLint: Herramienta para vigilar como escribimos el código

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
 /*Creación del servidor Node Express con ajustes básicos y visualizar*/
 
-import express from "express";
-import cors from "cors"; /*CORS es un paquete node.js para proporcionar un middleware Connect / Express que se puede usar para habilitar CORS con varias opciones.*/
-import compression from "compression";
-import { createServer } from "http";
+import express from 'express';
+import cors from 'cors'; /*CORS es un paquete node.js para proporcionar un middleware Connect / Express que se puede usar para habilitar CORS con varias opciones.*/
+import compression from 'compression';
+import { createServer } from 'http';
 import environment from './config/environments';
 
 // Configuracion de las variables del entorno (Lecturas)
