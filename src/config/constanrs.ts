6afdbb5490ca4793ef753f53d8ac34e309069485
Dokhos <mendozaluca5@outlook.com>
2020-09-19 21:14:54 -0300

    Variables de entorno: Configuración en las constantes palabra secreta

---
+++ src/config/constanrs.ts
@@ -0,0 +1,7 @@
+import environment from './environments';
+
+if (process.env.NODE_ENV !== 'production') {
+    const env = environment;
+}
+
+export const SECRET_KET = process.env.SECRET || 'LucaMendozaCursoGrophQLTiendaOnliine';
