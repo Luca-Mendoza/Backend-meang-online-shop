d82e8f79318eafdadafb5570a0c1a8a3cc7d3285
Dokhos <mendozaluca5@outlook.com>
2020-09-19 21:08:55 -0300

    Variables de entorno: crear fichero y configuraciones necesarias

---
+++ src/config/environments.ts
@@ -0,0 +1,15 @@
+import dotenv from 'dotenv';
+
+const environment = dotenv.config(
+    {
+        path: './src/.env'
+    }
+);
+
+if (process.env.NODE_ENV !== 'production'){
+    if (environment.error){
+        throw environment.error;
+    }
+}
+
+export default environment;
