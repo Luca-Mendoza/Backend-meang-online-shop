54364449b15fffbd2055b1084aeab2f91c6503a0
Dokhos <mendozaluca5@outlook.com>
2020-09-22 18:02:05 -0300

    Agregado Conexión de base de datos MongoDB

---
+++ src/lib/database.ts
@@ -0,0 +1,28 @@
+import chalk from 'chalk';
+import MongoClient from 'mongodb';
+
+class Database {
+    async init() {
+        const MONGO_DB = process.env.DATABASE || 'mongodb://localhost:27017/meang-online-shop';
+
+
+        const client = await MongoClient.connect(
+            MONGO_DB,
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true
+            }
+        );
+
+        const db = client.db();
+
+
+        if(client.isConnected()){
+            console.log('=============================DATABASE=============================');
+            console.log(`STATUS: ${chalk.greenBright('ONLINE')}`);
+            console.log(`STATUS: ${chalk.greenBright(db.databaseName)}`);
+        }
+    }
+}
+
+export default Database;
