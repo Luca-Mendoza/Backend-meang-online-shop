d5d3430589395e23a504673dc7dde420483799f6
Dokhos <mendozaluca5@outlook.com>
2020-10-01 18:26:36 -0300

    Modularizar Schema GramphQL - Instalación y configuración

--- src/interfaces/context.interface.ts
+++ src/interfaces/context.interface.ts
@@ -5,7 +5,7 @@ export interface Icontext {
 
 interface Irequest {
     headers: {
-        authorization : string;
+        authorization: string;
     };
 }
 
--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,8 +1,14 @@
 import 'graphql-import-node';
-import typeDefs from './schema.graphql';
 import resolvers from './../resolver/index';
 import { makeExecutableSchema } from 'graphql-tools';
 import { GraphQLSchema } from 'graphql';
+
+import { loadFilesSync } from '@graphql-tools/load-files';
+import { mergeTypeDefs } from '@graphql-tools/merge';
+
+
+const loadedFiles = loadFilesSync(`${__dirname}/**/*.graphql`);
+const typeDefs = mergeTypeDefs(loadedFiles);
 
 const schema: GraphQLSchema = makeExecutableSchema({
     typeDefs,
