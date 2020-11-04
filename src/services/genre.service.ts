e71e192e23e7b5c0690c70adc4f563034c3d9925
Dokhos <mendozaluca5@outlook.com>
2020-11-03 21:57:06 -0300

    Sé crea  el primer apartado - Services para obtener la informacion ' operaciones ' a dentro de los Resolvers

---
+++ src/services/genre.service.ts
@@ -0,0 +1,19 @@
+class GenresService extends ResolversOperationsService {
+    constructor(root: object, variables: object, context: object) {
+        super(root,variables,context);
+   
+    }
+
+    // Listar información
+    protected list(collection: string, listElement: string){
+        
+    }
+
+    // Obtener detalles del item
+
+    // Añadir item
+
+    // Modificar item
+
+    // Eliminar item
+}
---
+++ src/services/resolvers-operations.service.ts
@@ -0,0 +1,25 @@
+class ResolversOperationsService {
+
+    private root: object;
+    private variables: object;
+    private context: object;
+
+    constructor(root: object, variables: object, context: object) {
+        this.root = root;
+        this.variables = variables;
+        this.context = context;
+    }
+
+    // Listar información
+    protected list(collection: string, listElement: string){
+
+    }
+
+    // Obtener detalles del item
+
+    // Añadir item
+
+    // Modificar item
+
+    // Eliminar item
+}
