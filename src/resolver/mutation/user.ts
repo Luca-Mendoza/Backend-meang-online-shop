b827a960ecd285932631f050c73437b0dca73c46
Dokhos <mendozaluca5@outlook.com>
2020-12-18 21:30:17 -0300

    Se a añadido la funcionalidad para bloquear a los usuarios

--- src/resolver/mutation/user.ts
+++ src/resolver/mutation/user.ts
@@ -3,15 +3,18 @@ import UsersService from '../../services/users.service';
 
 const resolversUserMutation: IResolvers = {
     Mutation: {
-        async register(_, { user }, context) {
-            return new UsersService(_, {user}, context).register();
+        register(_, { user }, context) {
+            return new UsersService(_, { user }, context).register();
         },
-        async updateUser(_, {user}, context) {
-            return new UsersService(_, {user}, context).modify();
-        }, 
-        async deleteUser(_, {id}, context) {
-            return new UsersService(_, {id}, context).delete();
-        }, 
+        updateUser(_, { user }, context) {
+            return new UsersService(_, { user }, context).modify();
+        },
+        deleteUser(_, { id }, context) {
+            return new UsersService(_, { id }, context).delete();
+        },
+        blockGenre(_, { id }, context) {
+            return new UsersService(_, { id }, context).block();
+        }
     },
 };
 
--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -190,5 +190,27 @@ class UsersService extends ResolversOperationsService {
             message: result.message,
         };
     }
+    // Bloquear Usuario 
+    async block() {
+        const id = this.getVariables().id;
+        if (!this.checkData(String(id) || '')) {
+            return {
+                status: false,
+                message: 'El ID del usuario no se ha especificado correctamente.',
+                genre: null
+            };
+        }
+
+        const result = await this.update(this.collection, { id }, { active: false }, 'usuario');
+        return {
+            status: result.status,
+            message: (result.message) ? 'Bloqueado correctamente' : 'No se ha bloqueado comprobarlo por favor'
+        };
+
+    }
+    // Funciones 
+    private async checkData(value: string) {
+        return (value === '' || value === undefined) ? false : true;
+    }
 }
 export default UsersService;
