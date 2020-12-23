2075b9a6cedc6a3355aa19b15c0786aa71b57949
Dokhos <mendozaluca5@outlook.com>
2020-12-23 13:37:12 -0300

    1° - Activar usuario - Definición de Schema

--- src/interfaces/jwt.interface.ts
+++ src/interfaces/jwt.interface.ts
@@ -2,4 +2,6 @@ import { IUser } from './user.interface';
 
 export interface IJwt {
     user: IUser;
+    id: string;
+    email: string;
 }
--- src/resolver/mutation/email.ts
+++ src/resolver/mutation/email.ts
@@ -1,5 +1,7 @@
 import { IResolvers } from 'apollo-server-express';
+import { EXPIRETIME } from '../../config/constants';
 import { transport } from '../../config/mailer';
+import JWT from '../../lib/jwt';
 
 const resolversEmailMutation: IResolvers = {
     Mutation: {
@@ -22,6 +24,31 @@ const resolversEmailMutation: IResolvers = {
                         status: true,
                         message: 'Email correctamente enviado a' + mail.to,
                         mail
+                    });
+                });
+            });
+        },
+        async activeUserEmail(_, { id, email }) {
+            // Añadimos la llamada al servicio
+            console.log(email);
+            // Información para que el usuario pueda activar la cuenta - tiempo 1hs
+            const toke = new JWT().sign({id, email}, EXPIRETIME.H1);
+            return new Promise((resolve, reject) => {
+
+                transport.sendMail({
+                    from: '"🌹🍀🌹🌼🍀🌸<🥀Los.Jazmines🌷🍀🌼🌸> 🌹🍀" <floreria01.los.jazmines@gmail.com>', // sender address
+                    to: email, // list of receivers
+                    subject: 'Activar usuario', // Subject line---
+                    //text: `Hola`, // plain text body
+                    html: email.html, // html body
+                }, (error, _) => {
+                    (error) ? reject({
+                        status: false,
+                        messge: error
+                    }) : resolve({
+                        status: true,
+                        message: 'Email correctamente enviado a' + email.to,
+                        email
                     });
                 });
             });
