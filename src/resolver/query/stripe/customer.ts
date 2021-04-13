94471ab2dba08fab0a2bb1d4f7454f93a6ce7449
Dokhos <mendozaluca5@outlook.com>
2021-04-13 11:17:04 -0300

    Cliente - Query - Resolver - Lista - Dando solución a la definición de Obtener LIsta de cliente

--- src/resolver/query/stripe/customer.ts
+++ src/resolver/query/stripe/customer.ts
@@ -1,12 +1,41 @@
 import { IResolvers } from 'graphql-tools';
+import { IStripeCustomer } from '../../../interfaces/stripe/customer.interface';
 import StripeApi from '../../../lib/stripe-api';
 
 const resolversStipeCustomerQuery: IResolvers = {
-    Query: {
-        async customers(_, { limit }) {
-            console.log(limit);
-        }
-    },
+	Query: {
+		async customers(_, { limit }) {
+			console.log(limit);
+			const stripe = new StripeApi().stripe;
+			return await stripe.customers
+				.list({
+					limit,
+				}) // Respuesta satisfactoria (Result: Tipo de dato que nos esta devolviendo)
+				.then(
+					(result: {
+						// Valor traido de la API Stripe 'true' habrá más datos disponibles.
+						has_more: boolean;
+						// Datos disponibles del cliente
+						data: Array<IStripeCustomer>;
+					}) => {
+						return {
+							status: true,
+							message:
+								'Lista cargada corectamente con los clientes seleccionado',
+							hasMore: result.has_more,
+							customers: result.data,
+						};
+					},
+				)
+				.catch((error: Error) => {
+					return {
+						status: false,
+						message: 'Error: '.concat(error.message),
+						hasMore: false,
+					};
+				});
+		},
+	},
 };
 
 export default resolversStipeCustomerQuery;
