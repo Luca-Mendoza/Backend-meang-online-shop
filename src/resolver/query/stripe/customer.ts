135e661bd063dd0ebb1d804fa0ca262b89a5704a
Dokhos <mendozaluca5@outlook.com>
2021-04-13 12:05:55 -0300

    Cliente - Resolver - List - Aplicando paginación para obtener desde el untimo o antes que ciente teniamos, obteniendo esa información desde su ID

--- src/resolver/query/stripe/customer.ts
+++ src/resolver/query/stripe/customer.ts
@@ -4,12 +4,28 @@ import StripeApi from '../../../lib/stripe-api';
 
 const resolversStipeCustomerQuery: IResolvers = {
 	Query: {
-		async customers(_, { limit }) {
+		async customers(
+			_,
+			{ limit, startingAfter, endingBefore },
+		) {
 			console.log(limit);
+			let pagination;
+			if (startingAfter !== '' && endingBefore === '') {
+				pagination = { starting_after: startingAfter };
+			} else if (
+				startingAfter === '' &&
+				endingBefore !== ''
+			) {
+				pagination = { ending_before: endingBefore };
+			} else {
+				pagination = {};
+			}
+			console.log(pagination);
 			const stripe = new StripeApi().stripe;
 			return await stripe.customers
 				.list({
 					limit,
+					...pagination,
 				}) // Respuesta satisfactoria (Result: Tipo de dato que nos esta devolviendo)
 				.then(
 					(result: {
