03e5844a06926d397840324a91cc44ff2d765b51
Dokhos <mendozaluca5@outlook.com>
2021-04-14 10:04:34 -0300

    Clientes - Mutation - Resolver - Crear - Validación para evitar duplicados de email

--- src/resolver/mutation/stripe/customer.ts
+++ src/resolver/mutation/stripe/customer.ts
@@ -1,4 +1,5 @@
 import { IResolvers } from 'graphql-tools';
+import { IStripeCustomer } from '../../../interfaces/stripe/customer.interface';
 import StripeApi, {
 	STRIPE_ACTION,
 	STRIPE_OBJECTS,
@@ -7,6 +8,23 @@ import StripeApi, {
 const resolversStipeCustomerMutation: IResolvers = {
 	Mutation: {
 		async createCustomer(_, { name, email }) {
+			// Comprobar que el cliente no exista y en el caso de que exista,
+			// devolver diciendo que no se puede añadir
+			const userCheckExiste: {
+				data: Array<IStripeCustomer>;
+			} = await new StripeApi().execute(
+				STRIPE_OBJECTS.CUSTOMERS,
+				STRIPE_ACTION.LIST,
+				{ email },
+			);
+			if (userCheckExiste.data.length > 0) {
+				// Usuario Existe
+				return {
+					status: false,
+					message: `El usuario con el email ${email} ya existe en el sistema`,
+				};
+			}
+			// Crea un nuevo usuario en el sistema de stripe
 			return await new StripeApi()
 				.execute(
 					STRIPE_OBJECTS.CUSTOMERS,
