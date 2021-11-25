b3d15deb85659b28ec4a88fa2b6e9561936032ba
Dokhos <mendozaluca5@outlook.com>
2021-11-25 20:28:26 -0300

    Email

--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -2,30 +2,32 @@ import { transport } from '../config/mailer';
 import { IMailOpiotns } from '../interfaces/email.interface';
 
 class MailService {
-
-    send(mail: IMailOpiotns) {
-                  
-        return new Promise((resolve, reject) => {
-
-            transport.sendMail({
-                from: '"🌹🍀🌹🌼🍀🌸<🥀Los.Jazmines🌷🍀🌼🌸> 🌹🍀" <floreria01.los.jazmines@gmail.com>', // sender address
-                to: mail.to, // list of receivers
-                subject: mail.subject, // Subject line---
-                //text: `Hola`, // plain text body
-                html: mail.html, // html body
-            }, (error, _) => {
-                (error) ? reject({
-                    status: false,
-                    messge: error
-                }) : resolve({
-                    status: true,
-                    message: 'Email correctamente enviado a ' + mail.to,
-                    mail
-                });
-            });
-        });
-    }
-
+	send(mail: IMailOpiotns) {
+		return new Promise((resolve, reject) => {
+			transport.sendMail(
+				{
+					from:
+						'"🎮 Gamezonia Online Shop 🎮" <gamezoniashop@gmail.com>', // sender address
+					to: mail.to, // list of receivers
+					subject: mail.subject, // Subject line---
+					//text: `Hola`, // plain text body
+					html: mail.html, // html body
+				},
+				(error, _) => {
+					error
+						? reject({
+								status: false,
+								messge: error,
+						  })
+						: resolve({
+								status: true,
+								message: 'Email correctamente enviado a ' + mail.to,
+								mail,
+						  });
+				},
+			);
+		});
+	}
 }
 
 export default MailService;
