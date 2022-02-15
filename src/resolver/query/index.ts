62e06cadb85829d5d1aeef9913cca0a9b4229e0d
Luca-Mendoza <mendozaluca5@outlook.com>
2022-02-15 13:52:29 -0300

    ok

--- src/resolver/query/index.ts
+++ src/resolver/query/index.ts
@@ -1,17 +1,17 @@
-import resolversGenreQuery from './genre';
-import resolversShopProductsQuery from './shop-product';
-import queryStripeResolver from './stripe';
-import resolversTagQuery from './tag';
-import resolversUserQuery from './user';
+import resolversGenreQuery from "./genre";
+import resolversShopProductsQuery from "./shop-product";
+import queryStripeResolver from "./stripe";
+import resolversTagQuery from "./tag";
+import resolversUserQuery from "./user";
 
-const GMR = require('@wiicamp/graphql-merge-resolvers');
+const GMR = require("@wiicamp/graphql-merge-resolvers");
 
 const queryResolvers = GMR.merge([
-	resolversUserQuery,
-	resolversShopProductsQuery,
-	resolversGenreQuery,
-	resolversTagQuery,
-	queryStripeResolver,
+  resolversUserQuery,
+  resolversShopProductsQuery,
+  resolversGenreQuery,
+  resolversTagQuery,
+  queryStripeResolver,
 ]);
 
 export default queryResolvers;
